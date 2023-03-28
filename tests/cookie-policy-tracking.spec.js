689df2313be5c30c71be0253c0c807a582f4f406
Truong Dang <ddtblock1903@gmail.com>
2023-03-28 00:44:03 -0400

    green all please

--- tests/cookie-policy-tracking.spec.js
+++ tests/cookie-policy-tracking.spec.js
@@ -4,7 +4,7 @@ test('Site should set cookie consent cookie after user accepts cookies', async (
   await page.goto('http://localhost:3000')
   await page.waitForSelector('#cookie-consent', { state: 'visible' }) // Wait for cookie consent modal to appear
   await page.waitForSelector('#accept-button') // Wait for the I Agree button to appear
-  //  const button = await page.$('#accept-button')
+  // const button = await page.$('#accept-button')
   await page.waitForTimeout(1000) // Wait for the cookie to be set
   const cookies = await page.context().cookies()
   const cookieConsentCookie = cookies.find(cookie => cookie.name === 'cookie_consent')
